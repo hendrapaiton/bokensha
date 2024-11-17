419a2bd6e9e4ae15eed2cbd01cf9f4ceb9c2679c
Hendra Dwi Saputra <hendra@usa.com>
2024-11-18 06:27:32 +0700

    Add LoginView

--- apps/landing/views.py
+++ apps/landing/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import FormView
+from django.contrib.auth import authenticate, login
+from django.urls import reverse_lazy
+from .forms import LoginForm
 
-# Create your views here.
+class LoginView(FormView):
+    template_name = 'login.html'
+    form_class = LoginForm
+    success_url = reverse_lazy('success')
+
+    def form_valid(self, form):
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user = authenticate(self.request, username=username, password=password)
+        if user is not None:
+            login(self.request, user)
+            return super().form_valid(form)
+        else:
+            form.add_error(None, 'Invalid login credentials')
+            return self.form_invalid(form)
